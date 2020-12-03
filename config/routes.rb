134fa2d689e99036cec5f44083e6274100d725c9
michkles <michkles@gmail.com>
2020-12-03 08:40:24 +0100

    add endpoint to fetch spaces in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   get 'home/index'
 
   root 'home#index'
+
+  namespace :api do
+    namespace :v1 do
+      get 'bookings/index'
+      resources :spaces, :only => [:show]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
